2297f62e899bf6672af3752954dc74e50a3d8e2c
Kasberg0 <regiskouassi242@gmail.com>
2022-09-09 15:07:36 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
+    #include <stdio.h>
+
 /**
- *  * main - main block
- *   * Return: 0
- *    */
-int main(void)
-{
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+    int main(void)
+    {
+	    puts("\"Programmig is like building a multilingual puzzle");
+	    return (0);
+    }
