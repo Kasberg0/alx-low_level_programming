0a54abd76a3766528ef831ba67bc76a54fcfc714
kasberg0 <regiskouassi242@gmail.com>
2022-09-09 20:41:40 -0700

    Tasks 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - No entry point
+ *
+ * Return: Always 0 (Success!)
+ */
+
+int main(void)
+{
+	char low;
+
+	low = 'z';
+	while (low >= 'a')
+	{
+		putchar(low);
+		low--;
+	}
+	putchar('/n');
+
+	return (0);
+}
