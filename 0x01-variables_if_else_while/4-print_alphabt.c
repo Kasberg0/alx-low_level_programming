13bcbe2ba417bd338b7ebf4267d003f138bda51b
kasberg0 <regiskouassi242@gmail.com>
2022-09-09 21:51:23 -0700

    Tasks 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success!)
+ */
+
+int main(void)
+{
+	char low;
+
+	low = 'a';
+	while (low <= z)
+	{
+		if (low != 'e' && low != 'q')
+		{
+			putchar(low);
+		}
+		low++;
+	}
+	putchar('\n');
+	return (0);
+}
