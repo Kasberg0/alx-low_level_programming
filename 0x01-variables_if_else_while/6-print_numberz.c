759ab5b1e9b92be3da483f377e983b41c292dede
kasberg0 <regiskouassi242@gmail.com>
2022-09-09 20:54:15 -0700

    Tasks 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - No entry point
+ *
+ * Return: Always 0 (Success!)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
