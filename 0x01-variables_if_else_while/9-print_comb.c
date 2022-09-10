ea1a4d838703648d308ac9f15a95246daca15f84
kasberg0 <regiskouassi242@gmail.com>
2022-09-09 20:30:45 -0700

    Tasks 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - No entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int low;
+
+	for (low = 0; low <= 9; low++)
+	{
+		putchar(low + '0');
+		if (low < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
