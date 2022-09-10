040794d1766a3b4a1338ecbb9ebd62f5aa90fe60
kasberg0 <regiskouassi242@gmail.com>
2022-09-09 21:16:57 -0700

    Tasks 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <time.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - No entry point
+ *
+ * Return: Always 0 (Success!)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <=9; num++)
+	{
+		printf("%d", num);
+	}
+	putchar('\n');
+
+	return (0);
+}
