e4c3f1ed1b770e042298fb7f3574e6db8de244eb
kasberg0 <regiskouassi242@gmail.com>
2022-09-28 18:38:03 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,7 +17,8 @@ int helperFunction(int num, int pSqrt)
 		if ((pSqrt * pSqrt) > num)
 			return (-1);
 		else
-			return (helperFunction(num, pSqrt +1 ));
+			return (helperFunction(num, pSqrt +1));
+		
 	}
 }
 
