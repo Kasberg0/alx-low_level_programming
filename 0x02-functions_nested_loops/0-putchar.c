ddb2b35cdc8541579f63e16968acd8705e4e27f7
kasberg0 <regiskouassi242@gmail.com>
2022-10-17 06:16:14 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,44 +1,18 @@
-#include <unistd.h>
 #include "main.h"
-
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to  print
- *
- * Return: On success 1.
- * On error, -1 is returned, and error set appropriately.
+ * main - Print "Holberton" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
  */
 int main(void)
 {
-	char ch[8] = "_putchar";
-		int = (0);
-		char c;
+int c = 0;
+char holberton[10] = "_putchar\n";
 
-		while (i <= 8)
-		{
-			c = ch[i];
-			_putchar(c);
-			i++;
-		
-		
-		
-		}
-		_putchar("\n");
-		return(0);
-
-
-
-
-
-
-
-
-}
-
-
-
-
-int _putchar(char c)
+while (c < 9)
 {
-	return (write (1, &c, 1));
+_putchar(holberton[c]);
+c++;
+}
+return (0);
 }
